9efa8b77e0f3967e906f64a475a72501c5f55c11
kusuoki <nanmu.w4@gmail.com>
2019-09-18 16:32:56 +0200

    paddle class

---
+++ src/model/Paddle.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Paddle {
+
+    // temporary value, will see what works in graphics
+    public static final int HEIGHT = 200;
+    public static final int WIDTH = 50;
+    public static final int SPEED = 10;
+
+
+}
