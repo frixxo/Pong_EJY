37d3e323261659ca537bd3563fd128711d5d2658
kusuoki <nanmu.w4@gmail.com>
2019-09-18 16:46:13 +0200

    paddle and playarea

--- src/model/Paddle.java
+++ src/model/Paddle.java
@@ -7,5 +7,27 @@ public class Paddle {
     public static final int WIDTH = 50;
     public static final int SPEED = 10;
 
+    // default position
+    Vector position;
+    Paddle(int x, int y){
+        this.position = new Vector(x, y);
+    }
+
+
+    // get the paddles position
+    public int getX(){
+        return position.x;
+    }
+    public int getY(){
+        return position.y;
+    }
+
+    // move the paddles
+    public void setX(int x){
+        this.position.x = x;
+    }
+    public void setY(int y){
+        this.position.y = y;
+    }
 
 }
