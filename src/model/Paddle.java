563e51804276f3f930d063dbab53d296f97187b6
kusuoki <nanmu.w4@gmail.com>
2019-09-18 16:53:21 +0200

    paddle and playarea

--- src/model/Paddle.java
+++ src/model/Paddle.java
@@ -1,13 +1,13 @@
 package model;
 
 public class Paddle {
-
+    // controls the paddles that moves up and down
     // temporary value, will see what works in graphics
     public static final int HEIGHT = 200;
     public static final int WIDTH = 50;
     public static final int SPEED = 10;
 
-    // default position
+    // define the start position
     Vector position;
     Paddle(int x, int y){
         this.position = new Vector(x, y);
@@ -22,10 +22,7 @@ public class Paddle {
         return position.y;
     }
 
-    // move the paddles
-    public void setX(int x){
-        this.position.x = x;
-    }
+    // move the paddles, not allowed to move anywhere along the x-axel
     public void setY(int y){
         this.position.y = y;
     }
--- src/model/Vector.java
+++ src/model/Vector.java
@@ -1,11 +1,10 @@
 package model;
 
 public class Vector {
-
+    // simple class that saves a 2-dimensional position
     int x;
     int y;
-    Vector(int x, int y)
-    {
+    Vector(int x, int y) {
         this.x = x;
         this.y = y;
     }
