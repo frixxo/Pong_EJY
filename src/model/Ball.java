abfeee8e63d0349248ef5139c2c5646e56f15fdd
kusuoki <nanmu.w4@gmail.com>
2019-09-19 10:56:44 +0200

    simplified ball's move() method

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -36,28 +36,18 @@ public class Ball{
     *  k is the slope it will move on
     *  k = (y2 - y1) / (x2 - x1) , y2 and x2 are unknown
     *  if we assume at the next frame the ball moves 1 pixel at the x-axis
-    *  x2 = x1 + x_dir (it can go at both directions)
-    *  y2 = k * (x1 + x_dir - x1) + y1
-    *  y2 = k * x_dir + y1
-    *
-    *  note that y2 is the position, not the distance
+    *  then y2 = k + y1
+    *  also y_distance = k
     */
     public void move(double x_dir, double y_dir, double k) {
         x_dir = (x_dir == 0) ? 0 : (x_dir > 0) ? 1 : -1;
         y_dir = (y_dir == 0) ? 0 : (y_dir > 0) ? 1 : -1;
 
-        // k can never be 0, 1 gives a horizontal or vertical line
-        if (k == 0) k = 1;
-
-        double y_distance;
-        // if x_dir is 0, set y_distance as 1
-        // y_distance is always positive
-        y_distance = Math.abs((x_dir != 0) ? k * x_dir : 1);
+        double y_distance = Math.abs(k);
         if (y_distance > 1){
             // not allowing the y distance to surpass 1
-            double abs_y_dir = Math.abs(y_dir);
-            x_dir /= abs_y_dir;
-            y_distance /= abs_y_dir;
+            x_dir /= Math.abs(y_dir);
+            y_distance = 1;
         }
 
         this.position.x += this.SPEED * x_dir;
