fae0ff88bc404dfbbc265ad0ee741dd17a1f714a
Emil <35796281+ThePussyCat@users.noreply.github.com>
2019-10-15 17:23:30 +0200

    t

--- src/Graphics/Assets.java
+++ src/Graphics/Assets.java
@@ -22,8 +22,8 @@ import java.util.Map;
 
 public abstract class Assets {
 
-    private final String IMAGE_DIR = "file:assets/img/";
-    private final String SOUND_DIR = "file:assets/sound/";
+    private final String IMAGE_DIR = "file:Assets/img/";
+    private final String SOUND_DIR = "file:Assets/sound/";
 
     // A Map to store which image belongs to which object
     private final Map<Object, Image> objectImage = new HashMap<>();
