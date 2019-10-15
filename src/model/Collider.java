02b3e28ea4657b7b1f09d6a120d68b7055203e49
kusuoki <nanmu.w4@gmail.com>
2019-10-15 20:24:53 +0200

    probably fixed collision method, added pong class

--- src/model/Collider.java
+++ src/model/Collider.java
@@ -4,17 +4,17 @@ public class Collider {
 
     // static class methods, can be used without instantiating the class
     public static boolean isCollision(Ball ball, Paddle paddle) {
-        // checks the leftmost x value of ball and rightmost x value of paddle and vice versa
-        if ((ball.GetMovmentVector().x > 0 &&
-                ball.GetPosition().x + ball.GetSize().x >= paddle.GetPosition().x) ||
-                (ball.GetMovmentVector().x < 0 &&
-                        ball.GetPosition().x <= paddle.GetPosition().x + paddle.GetSize().x)) {
-            return true;
+        // checks if the ball collide with the paddle
+        if (ball.GetPosition().y - ball.GetSize().y > paddle.GetPosition().y ||
+            ball.GetPosition().y < paddle.GetPosition().y - paddle.GetSize().y ||
+                (ball.GetPosition().x > paddle.GetPosition().x + paddle.GetSize().x && ball.GetMovmentVector().x < 0) ||
+                (ball.GetPosition().x + ball.GetSize().x < paddle.GetPosition().x && ball.GetMovmentVector().x > 0)) {
+            return false;
         }
-        return false;
+        return true;
     }
 
-    public static boolean isCollision(Ball ball, int y) {
+    public static boolean isCollision(Ball ball, double y) {
         // checks only floor and ceiling
         // walls has nothing to do with collision
         // x,y starts at bottom-left, right?
